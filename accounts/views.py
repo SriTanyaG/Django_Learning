80db178adddb0109c1023bdaa59d973919cb30ff
SriTanyaG <sritanya.g-26@scds.saiuniversity.edu.in>
2026-01-07 14:39:43 +0530

    first commit

---
+++ accounts/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User, auth
+# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        email = request.POST['email']
+
+        if password1 == password2:
+            if User.onjects.filter(username=username).exists():
+                print('username taken')
+                messages.info(request,'username taken')
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                print('email taken')
+                messages.info(request,'email taken')
+                return redirect('register')
+            user = User.objects.create_user(username=username,password=password1,email=email,first_name=first_name,last_name=last_name)
+            user.save();
+            print('user created')
+            return redirect('/')
+    else: 
+        return render(request, 'register.html')
+def login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            print('login successful')
+            return redirect('/')
+        else:
+            print('invalid credentials')
+            return redirect('login')
+
+    return render(request, 'login.html')
---
+++ calc/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html',{'name':'ram'})
+
+def add(request):
+    x = int(request.POST['num1'])
+    y = int(request.POST['num2'])
+    res = x + y
+    return render(request, 'result.html',{'result':res})
